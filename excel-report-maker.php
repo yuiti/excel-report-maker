e4e790ce33f332af3f689ef5da6a3c1e194723b3
yuiti <y.abe@eyeta.jp>
2015-07-08 21:32:55 +0900

    wp scaffold

---
+++ excel-report-maker.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Excel-report-maker
+Version: 0.1-alpha
+Description: PLUGIN DESCRIPTION HERE
+Author: YOUR NAME HERE
+Author URI: YOUR SITE HERE
+Plugin URI: PLUGIN SITE HERE
+Text Domain: excel-report-maker
+Domain Path: /languages
+*/
+
